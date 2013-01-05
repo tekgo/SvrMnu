05af55ecf813a872170b266f4cc9f1cbca87ed92
Patrick Winchell <tekgo@tekgobook.local>
2013-01-05 09:38:41 -0600

    Minecraft parser

---
+++ ServerBowser/SvrBowMCServer.h
@@ -0,0 +1,43 @@
+//
+//  SvrBowSourceServer.h
+//  ServerBowser
+//
+//  Created by Patrick Winchell on 10/14/12.
+//  Copyright (c) 2012 Super Party Awesome. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "GCDAsyncSocket.h"
+
+static inline void readMCString(NSData *data,int *index,char* buffer)
+{
+    char thisChar='\xFF';
+    int i=0;
+    int thisIndex=index[0];
+    int lastChar=false;
+    while ((thisChar!='\x00' || !lastChar) && thisIndex<[data length]) {
+        if(thisChar=='\x00')
+            lastChar=true;
+        else
+            lastChar=false;
+        [data getBytes:&thisChar range:NSMakeRange(thisIndex, 1)];
+        if(thisChar!='\x00') {
+            buffer[i]=thisChar;
+            i++;
+        }
+        thisIndex++;
+    }
+    buffer[i]='\x00';
+    index[0]=thisIndex;
+    
+}
+
+@interface SvrBowMCServer : NSObject {
+        GCDAsyncSocket *Socket;
+    NSData *buffer;
+    
+}
+
+
+
+@end
